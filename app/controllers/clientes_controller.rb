d0be390a1256815d81a7a1034892e16ab618c03c
rubensmabueno <rubensmabueno@hotmail.com>
2013-06-24 21:17:29 -0300

    Correções do Brazilian-Rails

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,11 +1,6 @@
-class ClientesController < ApplicationController
+class ClientesController < InheritedResources::Base
   def index
     @clientes = Cliente.paginate(:page => params[:page], :per_page => 10)
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @clientes }
-    end
   end
 
   def show
@@ -14,52 +9,7 @@ class ClientesController < ApplicationController
     render :layout => false
   end
 
-  def new
-    @cliente = Cliente.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @cliente }
-    end
-  end
-
-  def edit
-    @cliente = Cliente.find(params[:id])
-  end
-
   def create
-    @cliente = Cliente.new(params[:cliente])
-
-    respond_to do |format|
-      if @cliente.save
-        format.html { render action: 'index', notice: 'Cliente foi criado com sucesso.' }
-      else
-        format.html { render action: "new" }
-      end
-    end
-  end
-
-  def update
-    @cliente = Cliente.find(params[:id])
-
-    respond_to do |format|
-      if @cliente.update_attributes(params[:cliente])
-        format.html { redirect_to @cliente, notice: 'Cliente foi editado com sucesso.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @cliente.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @cliente = Cliente.find(params[:id])
-    @cliente.destroy
-
-    respond_to do |format|
-      format.html { redirect_to clientes_url }
-      format.json { head :no_content }
-    end
+    super
   end
 end
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,13 +1,15 @@
 class Cliente < ActiveRecord::Base
-  attr_accessible :cpf, :data_nascimento, :etnia_id, :nome, :observacao, :rg, 
+  attr_accessible :cpf, :data_nascimento, :etnia_id, :nome, :observacao, :rg,
   :sexo_id, :enderecos_attributes, :emails_attributes, :telefones_attributes
+
+  validates_presence_of :sexo_id, :etnia_id, :nome, :cpf
 
 	belongs_to :etnia
 	belongs_to :sexo
 
 	has_many :enderecos, :as => :enderecable
   	accepts_nested_attributes_for :enderecos
-  
+
 	has_many :telefones, :as => :telefonable
   	accepts_nested_attributes_for :telefones
 
