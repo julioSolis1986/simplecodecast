a555b7a17a94a07ecafca58629cf61d542d10120
Rubens Minoru Andako Bueno <rubensmabueno@hotmail.com>
2014-06-06 23:48:26 -0300

    Add the new files from a updated devise

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '92aa583b7bcbea0b1052ad0d0ef42a5354963e59e719b6e4cd9aecfe2ab6bac22a0ca14ceb20075254145d8db2092b984ccb3540b707936118d55907ff93be14';
